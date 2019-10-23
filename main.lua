9f3f6f8a98a347e751ef6ed33b21c39e24899509
viktorli <viktorli@tencent.com>
2019-10-23 17:23:55 +0800

    111

--- main.lua
+++ main.lua
@@ -4,11 +4,18 @@ local mp = UnitPower("player")
 local noticeString = "我档期的血量为"..hp.."我档期的mp是"..mp
 
 -- 获取当前包裹的物品清单
-function getMyBagsItems() 
+function getMyBagsItems()
     print("get my bag's items")
 end
 
-SLASH_NOTICE1="/notice"
+function save()
+    print("save~~~~~~")
+end
+function load()
+    print("load~~~~~~")
+end
+
+SLASH_NOTICE1="/supplybag"
 SlashCmdList["NOTICE"]=function(cmd)
     SendChatMessage("cmd:"..cmd,"say")
     print("cmd:"..cmd)
